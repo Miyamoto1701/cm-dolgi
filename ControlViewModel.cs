4a38a51cc0d9a9528e5348a6b9262d74bdeb4446
Miyamoto1701 <artemlichkov@gmail.com>
2026-04-22 13:09:18 +0300

    Add files via upload

---
+++ ControlViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractWork2.ViewModels
+{
+    public partial class ControlViewModel : ViewModelBase
+    {
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(Age))]
+        private DateTime selectedDate;
+
+        public int Age => DateTime.Now.Month >= selectedDate.Month && DateTime.Now.Day >= selectedDate.Day ?
+            DateTime.Now.Year - selectedDate.Year :
+            DateTime.Now.Year - selectedDate.Year- 1;
+    }
+}
---
+++ ControlsView.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace PractWork2.Views;
+
+public partial class ControlsView : UserControl
+{
+    public ControlsView()
+    {
+        InitializeComponent();
+
+    }
+}
